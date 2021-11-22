49d361effaa40d9353f63706bcfaa574a75d94c0
Leonardo Benitez <benitez.leonardo.py@gmail.com>
2021-11-22 18:41:25 -0300

    Challenge #5

--- redisolar/dao/redis/site_geo.py
+++ redisolar/dao/redis/site_geo.py
@@ -73,6 +73,21 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
         site_ids: List[str] = []
         scores: Dict[str, float] = {}
 
+        site_geo_key = self.key_schema.site_geo_key()
+        site_ids = self.redis.georadius(
+            site_geo_key,
+            query.coordinate.lng,
+            query.coordinate.lat,
+            query.radius,
+            unit=query.radius_unit.value,
+        )
+
+        capacity_ranking_key = self.key_schema.capacity_ranking_key()
+        for site_id in site_ids:
+            p.zscore(capacity_ranking_key, site_id)
+
+        scores = dict(zip(site_ids, p.execute()))
+
         for site_id in site_ids:
             if scores[site_id] and scores[site_id] > CAPACITY_THRESHOLD:
                 p.hgetall(self.key_schema.site_hash_key(site_id))
